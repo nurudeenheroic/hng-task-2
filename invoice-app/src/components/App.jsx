3b78666b6639d4f4f09a9742339107a32395eaf9
Luminus <nurudeenexpert@gmai.com>
2026-04-22 01:41:59 +0100

    moved into a folder and created a sidebar

---
+++ invoice-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import { Sidebar } from './sidebar.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+    </div>
+  );
+}
+export default App
---
+++ invoice-app/src/components/sidebar.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './sidebar.css'
+
+
+export function Sidebar () {
+    return (
+    <>  
+    <div className='side-bar'>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+    </>
+    )
+}
