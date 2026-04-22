bbb1a929d693c636e2262e93241f2fd2e9aeffac
Luminus <nurudeenexpert@gmai.com>
2026-04-22 02:48:26 +0100

    added new component and rearranged it

--- invoice-app/src/components/App.jsx
+++ invoice-app/src/components/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import { Sidebar } from './sidebar.jsx'
+import { MainContent } from './mainContent.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar/>
-    </div>
+    <>
+        <div className="App">
+             <Sidebar/>
+        </div>
+        <div>
+            <MainContent/>
+        </div>
+    </>
   );
 }
 export default App
