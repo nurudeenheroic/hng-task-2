514a026decfa86865a10158c1743380508905500
Luminus <nurudeenexpert@gmai.com>
2026-04-22 02:38:48 +0100

    styled the sidebar component

--- invoice-app/src/components/sidebar.jsx
+++ invoice-app/src/components/sidebar.jsx
@@ -4,7 +4,6 @@ import invoiceProfile from '../assets/invoiceprofile.png'
 import Logo from '../assets/invoicelogo.png'
 import './sidebar.css'
 
-
 export function Sidebar () {
     return ( 
     <aside className='side-bar'>
