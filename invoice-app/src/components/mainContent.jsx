94ad605443652cf2ba37f2417a25c8a02abd6a2f
Luminus <nurudeenexpert@gmai.com>
2026-04-22 02:45:29 +0100

    set up mainContent component

---
+++ invoice-app/src/components/mainContent.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './mainContent.css'
+
+export function MainContent () {
+    return (
+        <main className='main-content'>
+            
+        </main>
+    )
+}
