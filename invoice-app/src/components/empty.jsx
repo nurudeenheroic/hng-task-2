5ab8d0c3fbbfb3f6131a7d7b6533bcdc7750537c
Luminus <nurudeenexpert@gmai.com>
2026-04-22 13:15:09 +0100

    separated empty  as a compnoent

---
+++ invoice-app/src/components/empty.jsx
@@ -0,0 +1,14 @@
+import './empty.css'
+import EmptyDisplay from '../assets/Email campaign_Flatline 2.png'
+
+export function Empty() {
+    return (
+        <div className='empty'>
+                    <figure>
+            <img src={EmptyDisplay} alt="No invoices to display "/>
+                    </figure>
+                    <h3>There is nothing here</h3>
+                    <p>Create an invoice by clicking the <br /> "New Invoice" button and get started</p>
+        </div>
+    )
+}
