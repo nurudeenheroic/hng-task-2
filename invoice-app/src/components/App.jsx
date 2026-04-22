4f7a0fc10b38c0726bdf41fc4b0d00e38d1f6ee5
Luminus <nurudeenexpert@gmai.com>
2026-04-22 02:49:51 +0100

    edit for proper semantics

--- invoice-app/src/components/App.jsx
+++ invoice-app/src/components/App.jsx
@@ -5,14 +5,14 @@ import { MainContent } from './mainContent.jsx';
 
 function App() {
   return (
-    <>
+    <div className="App">
         <div className="App">
              <Sidebar/>
         </div>
         <div>
             <MainContent/>
         </div>
-    </>
+    </div>
   );
 }
 export default App
