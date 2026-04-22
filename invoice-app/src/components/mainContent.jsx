54585d4af56d001e866e50fe79464415ae6c8ac7
Luminus <nurudeenexpert@gmai.com>
2026-04-22 03:09:41 +0100

    linked google font

--- invoice-app/src/components/mainContent.jsx
+++ invoice-app/src/components/mainContent.jsx
@@ -4,7 +4,14 @@ import './mainContent.css'
 export function MainContent () {
     return (
         <main className='main-content'>
-            
+            <header className='main-header'>
+            <div>
+                
+            </div>
+            </header>
+            <div className='empty'>
+
+            </div>
         </main>
     )
 }
