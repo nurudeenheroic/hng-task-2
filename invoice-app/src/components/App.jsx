51910b36b2d6201e9bb07534f7fb7e04aefac1d8
Luminus <nurudeenexpert@gmai.com>
2026-04-23 04:36:12 +0100

    added new route and import

--- invoice-app/src/components/App.jsx
+++ invoice-app/src/components/App.jsx
@@ -1,10 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { Routes, Route } from 'react-router'
-import HomePage from './homePage.jsx';
+import HomePage from './homePage.jsx'
+import { PreviewPage } from './previewPage.jsx'
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/invoices/:id" element={<PreviewPage />}></Route>
     </Routes>
   )
 }
