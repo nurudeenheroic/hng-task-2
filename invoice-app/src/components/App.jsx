334f9518019d97c01ce6254a6c35c2100ee0a8fb
spe-ctre <superadmin@tyap.com>
2026-04-24 00:17:26 +0100

    added new component with route

--- invoice-app/src/components/App.jsx
+++ invoice-app/src/components/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './homePage.jsx'
 import { PreviewPage } from './previewPage.jsx'
+import { NewInvoicePage } from './newInvoicePage.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/invoices/:id" element={<PreviewPage />}></Route>
+      <Route path="/new-invoice" element={<NewInvoicePage />}></Route>
     </Routes>
   )
 }
